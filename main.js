91fe887040034749d620bd395de7600e6e337dd5
yauheni-beiduk <yauheni.beiduk@gmail.com>
2020-11-19 17:50:35 +0300

    fix:add js function

--- main.js
+++ main.js
@@ -8,19 +8,37 @@ const app = () => {
     const sounds = document.querySelectorAll('.sound-picker button');
     // TIME DISPLAY 
     const timeDisplay = document.querySelector('.time-display');
+    const timeSelect = document.querySelectorAll('.time-select button');
     // GET THE LENGTH OFF THE OUTLINE
     const outlineLength = outline.getTotalLength();
     // DURATION
     let fakeDuration = 600;
 
-    outline.style.strokeDasharray = outlineLength;
+    outline.style.strokeDasharray = outlineLength;  // chertochki for svg form
     outline.style.strokeDashoffset = outlineLength;
+
+    //PICK DIFFERENT SOUNDS
+    sounds.forEach(sound => {
+        sound.addEventListener('click', function() {
+            song.src = this.getAttribute('data-sound');
+            video.src = this.getAttribute('data-video');
+            checkPlaying(song);
+        });
+    });
 
     // PLAY SOUNDS
     play.addEventListener('click', () => {
         checkPlaying(song);
     });
     
+    //SELECT SOUND
+    timeSelect.forEach(option => {
+        option.addEventListener('click', function() {
+            fakeDuration = this.getAttribute('data-time');
+            timeDisplay.textContent = `${Math.floor(fakeDuration / 60)}:${Math.floor(fakeDuration % 60)} `;
+        });
+    });
+
     // CREATE A FUNCTION SPECIFIC TO STOP AND PLAY THE SOUNDS
     const checkPlaying = song => {
     if(song.paused) {
@@ -30,12 +48,30 @@ const app = () => {
     } else {
         song.pause();
         video.pause();
-        play.src = 'images/play.svg'
+        play.src = 'images/play.svg';
     }
-}
+    };
+
+
+  // ANIMATED CIRCLE TO TIMES
+    song.ontimeupdate = () => {
+        let currentTime = song.currentTime;
+        let elapsed = fakeDuration - currentTime;
+        let seconds = Math.floor(elapsed % 60);
+        let minutes = Math.floor(elapsed / 60);
+
+        // ANIMATE CIRCLE
+        let progress = outlineLength - (currentTime / fakeDuration) * outlineLength;
+        outline.style.strokeDashoffset = progress;
+        // ANIMATE THE TEXT
+        timeDisplay.textContent = `${minutes}:${seconds}`;
+
+        if(currentTime >= fakeDuration) {
+            song.pause();
+            song.currentTime = 0;
+            play.src = 'images/play.svg';
+            video.pause();
+        }
+    };
 };
-
-
-
-
 app();
